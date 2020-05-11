d93d0a6f23b2a2c4e036d66ea75d7483bea54223
PanAlle <217328@studenti.unimore.it>
2020-05-11 16:29:19 +0200

    Base format

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
+#include <ctype.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+void encode(const char *original, char *encoded){
+    //TO DO
+}
+
+void decode(const char *encoded, char *decoded){
+    //TO DO
+}
+
+void main(void ) {
+    const char original[100] = "WWWWWWWWWWWWBWWWWWWWWWWWWBBBWWWWWWWWWWWWWWWWWWWWWWWWB";
+    char encoded [100];
+    char decoded [100];
+    printf("Original: %s\n", original);
+    encode(original , encoded);
+    printf("Encoded: %s\n", encoded);
+    decode(encoded, decoded);
+    printf("Decoded: %s\n", decoded);
 }
